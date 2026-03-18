2157ec231810e2fc10fed9b3450e360f62036373
Vishmi Amandi <vishwijesooriya@gmail.com>
2026-03-18 22:52:21 +0530

    created tailwind.config.js and postcss.config.js files in client

---
+++ Client/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FFCB05',
+        secondary: '#332421',
+      }
+    },
+  },
+  plugins: [],
+}
