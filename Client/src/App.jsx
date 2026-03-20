db3a33fe452c4c09d2349f87e39549c323674bc8
Vishmi Amandi <vishwijesooriya@gmail.com>
2026-03-20 22:15:25 +0530

    update app.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { auth } from './firebase';
 import Login from './modules/user/pages/login';
+import Dashboard from './modules/user/pages/dashboard';
+
+// Protected Route component
+const ProtectedRoute = ({ children }) => {
+  const user = auth.currentUser;
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route 
+          path="/dashboard" 
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } 
+        />
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
